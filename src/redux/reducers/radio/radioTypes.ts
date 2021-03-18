206fdaa8346d16e063034e1ffdfa98bf48985c6a
solomonakp <solomonakp@gmail.com>
2021-03-18 23:52:12 +0100

    quick update

--- src/redux/reducers/radio/radioTypes.ts
+++ src/redux/reducers/radio/radioTypes.ts
@@ -2,7 +2,6 @@ import { StationProps } from '../../../stationsObject';
 export const TOGGLE_STATIONS_LOADING = 'TOGGLE_STATIONS_LOADING';
 export const SET_STATION = 'SET_STATION';
 export const GET_STATIONS = 'GET_STATIONS';
-export const SET_CURRENT_STATION = 'SET_CURRENT_STATION';
 
 interface ToggleLoadingStationsAction {
   type: typeof TOGGLE_STATIONS_LOADING;
@@ -17,11 +16,6 @@ interface GetStationsAction {
   payload: Array<StationProps>;
 }
 
-interface setCurrentStationAction {
-  type: typeof SET_CURRENT_STATION;
-  payload: string;
-}
-
 export interface RadioState {
   isLoading: boolean;
   stations: Array<StationProps> | [];
@@ -33,5 +27,4 @@ export interface RadioState {
 export type RadioActionTypes =
   | ToggleLoadingStationsAction
   | SetStationAction
-  | GetStationsAction
-  | setCurrentStationAction;
+  | GetStationsAction;
